55b0ebeb26230b3c405a9c0b562607af4bd5ad68
zoojar <davidnewton82@gmail.com>
2016-12-08 17:51:26 +0000

    init aio

---
+++ install_aio_mom_on_centos7.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+#Defaults:
+log_file='/tmp/install_aio_mom.log'
+conf_file='/tmp/master.conf'
+installer_stagedir='/tmp/peinstaller'
+installer_file="${installer_stagedir}/puppet-enterprise-installer"
+r10k_key_path="/etc/puppetlabs/puppetserver/ssh"
+r10k_key_file="id-control_repo.rsa"
+r10k_remote="https://github.com/zoojar/control-repo"
+hiera_yaml_file="/etc/puppetlabs/hiera.yaml"
+console_admin_password="puppet"
+peinstaller_url='https://pm.puppetlabs.com/puppet-enterprise/2016.2.0/puppet-enterprise-2016.2.0-el-7-x86_64.tar.gz'
+puppetmaster_fqdn="$(hostname -f)"
+
+firewall_default_zone=`sudo firewall-cmd --get-default-zone`
+echo "$(date) INFO: Configuring firewall: Opening ports 8140, 443, 61613 & 8142 for the default zone: ${firewall_default_zone}..." >> $log_file
+firewall-cmd --permanent --zone=$firewall_default_zone --add-port=8140/tcp
+firewall-cmd --permanent --zone=$firewall_default_zone --add-port=443/tcp
+firewall-cmd --permanent --zone=$firewall_default_zone --add-port=61613/tcp
+firewall-cmd --permanent --zone=$firewall_default_zone --add-port=8142/tcp
+firewall-cmd --permanent --zone=$firewall_default_zone --add-port=4433/tcp
+firewall-cmd --reload
+
+echo "$(date) INFO: Downloading puppet from $peinstaller_url and extracting to $installer_stagedir..." >> $log_file
+yum -y install wget
+wget -q --timeout=1200 $peinstaller_url -O /tmp/peinstaller.tar.gz
+mkdir $installer_stagedir
+tar -xf /tmp/peinstaller.tar.gz --strip-components=1 -C $installer_stagedir
+
+
+echo "$(date) INFO: Preparing the install config file..." >> $log_file
+cat <<EOF > $conf_file
+{
+  "console_admin_password": "$console_admin_password",
+  "puppet_enterprise::puppet_master_host": "$puppetmaster_fqdn",
+  "puppet_enterprise::use_application_services": true,
+  "puppet_enterprise::profile::master::code_manager_auto_configure": true,
+  "puppet_enterprise::profile::master::r10k_remote": "$r10k_remote",
+  "puppet_enterprise::profile::master::r10k_private_key": "$r10k_key_path/$r10k_key_file"
+}
+EOF
+
+echo "$(date) INFO: Installing puppet..." >> $log_file
+sudo $installer_file -c $conf_file
+
+echo "$(date) INFO: Configuring code manager..." >> $log_file
+PATH="/opt/puppetlabs/bin:/opt/puppetlabs/puppet/bin:/opt/puppet/bin:$PATH"
+puppet agent -t
+puppet module install npwalker-pe_code_manager_webhook --version 1.0.11
+chown -R pe-puppet:pe-puppet /etc/puppetlabs/code/
+puppet apply -e "include pe_code_manager_webhook::code_manager"
+
+echo "$(date) INFO: Setting console admin password..." >> $log_file
+PATH="/opt/puppetlabs/bin:/opt/puppetlabs/puppet/bin:/opt/puppet/bin:$PATH"
+/opt/puppetlabs/puppet/bin/ruby /opt/puppetlabs/server/data/enterprise/modules/pe_install/files/set_console_admin_password.rb $console_admin_password
+
+
---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "vsphere_user"       {}
+variable "vsphere_password"   {}
+variable "vsphere_server"     {}
+variable "remote_exec_script" { default = "install_aio_mom_on_centos7.sh"}
+variable "dns_servers"        { default = [ "8.8.8.8", "192.168.0.1" ] }
+variable "gateway"            { default = "192.168.0.1" }
+variable "datacenter"         { default = "Datacenter 1" }
