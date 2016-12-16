98c14b352841920ea70c2f7782a9fe5757e84328
zoojar <davidnewton82@gmail.com>
2016-12-16 15:57:07 +0000

    env var for installer

--- variables.tf
+++ variables.tf
@@ -5,3 +5,4 @@ variable "remote_exec_script" { default = "install_aio_mom_on_centos7.sh"}
 variable "dns_servers"        { default = [ "8.8.8.8", "192.168.0.1" ] }
 variable "gateway"            { default = "192.168.0.1" }
 variable "datacenter"         { default = "Datacenter 1" }
+variable "pe_installer_url"   {}
