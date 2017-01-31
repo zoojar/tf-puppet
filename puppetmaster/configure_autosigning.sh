d055c7b959c490c540e337083da800d2b5ab7577
zoojar <davidnewton82@gmail.com>
2017-01-31 23:29:52 +0000

    autosigning script

---
+++ puppetmaster/configure_autosigning.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Script to configure autosigning on PE 2016.5.1. Accepts one argument: the PSK - written into /etc/puppetlabs/puppet/global-psk.
+
+psk=$1
+autosign_exe_url="https://raw.githubusercontent.com/zoojar/classified/master/autosign.sh"
+
+echo "Configuring policy-based autosigning..."
+echo $psk >/etc/puppetlabs/puppet/global-psk
+curl -L "${autosign_exe_url}" > /etc/puppetlabs/puppet/autosign.sh
+sudo chmod 500 /etc/puppetlabs/puppet/autosign.sh ; sudo chown pe-puppet /etc/puppetlabs/puppet/autosign.sh
+sudo puppet config set autosign /etc/puppetlabs/puppet/autosign.sh --section master
