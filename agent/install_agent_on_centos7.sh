2e9cc82db70ed3571e315bfad3bb454d3daa7d28
zoojar <davidnewton82@gmail.com>
2017-02-01 15:28:20 +0000

    added role param for csr

--- agent/install_agent_on_centos7.sh
+++ agent/install_agent_on_centos7.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 #Script to install PE Puppet Agent via frictionless installer and autosigning.
-#Accepts 3 arguments...
+#Accepts 4 arguments...
 master_ip="$1"
 master_fqdn="$2"
 psk="$3"
+role="$4"
 
 echo "Adding [$master_ip $master_fqdn] to hosts file..."
 sudo echo -e "$master_ip $master_fqdn\n$(cat /etc/hosts)" > /etc/hosts
 
 echo "Setting up custom csr attributes for autosigning..."
 mkdir -p /etc/puppetlabs/puppet
-printf "custom_attributes:\n  1.2.840.113549.1.9.7: $psk" >  /etc/puppetlabs/puppet/csr_attributes.yaml 
-
+printf "custom_attributes:\n  1.2.840.113549.1.9.7: $psk\nextension_requests:\n  pp_role: $role\n" >  /etc/puppetlabs/puppet/csr_attributes.yaml 
 echo "Installing puppet from https://$master_fqdn:8140/packages/current/install.bash..."
 curl -k https://$master_fqdn:8140/packages/current/install.bash | sudo bash
 
