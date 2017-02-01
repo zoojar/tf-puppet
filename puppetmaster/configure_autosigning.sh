19c30a6bdf1ed6cfd383b1853b255595a766b4e5
zoojar <davidnewton82@gmail.com>
2017-02-01 00:02:36 +0000

    autosigning script

--- puppetmaster/configure_autosigning.sh
+++ puppetmaster/configure_autosigning.sh
@@ -7,5 +7,6 @@ autosign_exe_url="https://raw.githubusercontent.com/zoojar/classified/master/aut
 echo "Configuring policy-based autosigning..."
 echo $psk >/etc/puppetlabs/puppet/global-psk
 curl -L "${autosign_exe_url}" > /etc/puppetlabs/puppet/autosign.sh
-sudo chmod 500 /etc/puppetlabs/puppet/autosign.sh ; sudo chown pe-puppet /etc/puppetlabs/puppet/autosign.sh
-sudo puppet config set autosign /etc/puppetlabs/puppet/autosign.sh --section master
+chmod 500 /etc/puppetlabs/puppet/autosign.sh ; sudo chown pe-puppet /etc/puppetlabs/puppet/autosign.sh
+PATH="/opt/puppetlabs/bin:/opt/puppetlabs/puppet/bin:/opt/puppet/bin:$PATH"
+puppet config set autosign /etc/puppetlabs/puppet/autosign.sh --section master
